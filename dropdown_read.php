7838ce9f6761b084f8b6cc355b729be68e3d147f
AMMcIT <162496545+AMMcIT@users.noreply.github.com>
2024-11-13 17:06:35 -0500

    Add files via upload
    Fixed SQL query, now actually generates the array. Javascript still needs work.

--- dropdown_read.php
+++ dropdown_read.php
@@ -5,17 +5,23 @@ session_start();
 include 'connect.php';
 
 $chat_id = $_POST['chat_id'];
+#$chat_id = '1';
 
 $sql = "SELECT `content` 
-        FROM `messages` 
+        FROM `message` 
         WHERE `chat_id` = '$chat_id'
         ORDER BY chat_id";
 
 $result = $conn->query($sql);
 
+if ($result === false) {
+    die("Error: " . $conn->error);
+}
+
 $message_array = array();
 
 while ($row_values = $result->fetch_assoc()) {
+    $echo = $row_values['content'];
     $message_array[] = $row_values['content'];
 }
 
