719e7eadb6e4a20c460212bbce64bdbd89616a74
Sriram Nair <srirambnair@gmail.com>
2024-12-01 20:36:07 -0500

    Going back to previous version
    Sending messages wasn't working so I removed the alert system

--- signup.php
+++ signup.php
@@ -10,31 +10,44 @@ $sql = "SELECT `uuid` FROM `users`
         WHERE `username` = '$uname'";
 $result = $conn->query($sql);
 
+echo gettype($result);
 $tresult = mysqli_fetch_assoc($result);
+echo "$tresult";
 
-if ($psw != $cpsw) {
-    echo "<script type='text/javascript'>alert('Passwords do not match'); window.location.href = 'SignupForm.html';</script>";
+if ($psw != $cpsw){
+    $message = "PASSWORDS DO NOT MATCH";
+    echo "<script type='text/javascript'>alert('$message');</script>";
+    header('Location: SignupForm.html');
     exit;
 }
 if ($result->num_rows != 0) {
-    echo "<script type='text/javascript'>alert('That username already exists'); window.location.href = 'SignupForm.html';</script>";
+    $message = "USERNAME TAKEN";
+    echo "<script type='text/javascript'>alert('$message');</script>";
+    header('Location: SignupForm.html');
     exit;
 }
-
-if (isset($_POST["uname"], $_POST["psw"])) {
-    $sql = "INSERT INTO Users (username, password_hash) VALUES (?, ?)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param('ss', $uname, $psw);
-
-    if ($stmt->execute()) {
-        echo "<script type='text/javascript'>alert('Signup successful! You can log in now.'); window.location.href = 'Login.html';</script>";
-        exit;
+if ($psw == $cpsw and $result->num_rows == 0){
+    if (isset($_POST["uname"], $_POST["psw"])) {
+        echo "Received data: uname = " . $_POST["uname"] . ", psw = " . $_POST["psw"];
     } else {
-        echo "<script type='text/javascript'>alert('Error during signup. Please try again.'); window.location.href = 'SignupForm.html';</script>";
+        $message = "POST data not received properly";
+        echo "<script type='text/javascript'>alert('$message');</script>";
+        header('Location: SignupForm.html');
         exit;
     }
-} else {
-    echo "<script type='text/javascript'>alert('Invalid input. Please try again.'); window.location.href = 'SignupForm.html';</script>";
-    exit;
+    $message = "YOU DID IT. LOG IN NOW";
+    echo "<script type='text/javascript'>alert('$message');</script>";
 }
-?>
+$sql = "INSERT INTO Users (username, password_hash) VALUES (?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param('ss', $uname, $psw);
+
+if ($stmt->execute()) {
+    echo "User inserted successfully.";
+} else {
+    echo "Error inserting User: " . $stmt->error;
+}
+$stmt->close();
+header('Location: Login.html');
+exit;
+#$verify_sql = "SELECT chat_id FROM Message WHERE '$uname' = ";
